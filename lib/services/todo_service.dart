10dc618f72766ee68284048d5b0e2c705fbec6ec
Guilherme Tavares <contato@guilhermeomt.dev>
2023-09-01 11:05:46 -0300

    feat: todo service

--- lib/services/todo_service.dart
+++ lib/services/todo_service.dart
@@ -1,3 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sqlite_todo/database/repositories/todo_repository.dart';
+import 'package:flutter_sqlite_todo/models/todo.dart';
 
-class TodoService with ChangeNotifier {}
+class TodoService with ChangeNotifier {
+  final _todoRepository = TodoRepository();
+  List<Todo> _todos = [];
+
+  List<Todo> get todos => _todos;
+
+  Future<String> getTodos(String username) async {
+    try {
+      _todos = await _todoRepository.getAllTodos(username);
+      notifyListeners();
+    } catch (e) {
+      return e.toString();
+    }
+    return "OK";
+  }
+
+  Future<String> deleteTodo(Todo todo) async {
+    try {
+      await _todoRepository.deleteTodo(todo);
+    } catch (e) {
+      return e.toString();
+    }
+    String result = await getTodos(todo.username);
+    return result;
+  }
+
+  Future<String> createTodo(Todo todo) async {
+    try {
+      await _todoRepository.createTodo(todo);
+    } catch (e) {
+      return e.toString();
+    }
+    String result = await getTodos(todo.username);
+    return result;
+  }
+
+  Future<String> toggleTodoDone(Todo todo) async {
+    try {
+      await _todoRepository.toggleTodoDone(todo);
+    } catch (e) {
+      return e.toString();
+    }
+    String result = await getTodos(todo.username);
+    return result;
+  }
+}
